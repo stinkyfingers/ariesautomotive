4c47b2bdc36cb0a5cf6b6217df1139e05a2fbb78
Alex Ninneman <alex@ninneman.org>
2016-02-09 12:36:57 -0600

    made the video header mobile friendly

--- src/components/home-navbar/navbar.controller.js
+++ src/components/home-navbar/navbar.controller.js
@@ -1,19 +1,18 @@
 'use strict';
 
+var videos = [{
+	poster: '//storage.googleapis.com/aries-website/hero_video/whitewashed_poster.jpg',
+	files: [{
+		path: '//storage.googleapis.com/aries-website/hero_video/SampleVideoForSite_ARIES.mp4',
+		type: 'video/mp4',
+	},{
+		path: '//storage.googleapis.com/aries-website/hero_video/SampleVideoForSite_ARIES.webm',
+		type: 'video/webm',
+	}]
+}];
+
 angular.module('ariesautomotive').controller('HomepageNavController', ['$scope', '$location', '$state', function ($scope, $location, $state) {
 	$scope.search_term = '';
-
-	var videos = [{
-		poster: '//storage.googleapis.com/aries-website/hero_video/whitewashed_poster.jpg',
-		files: [{
-			path: '//storage.googleapis.com/aries-website/hero_video/SampleVideoForSite_ARIES.mp4',
-			type: 'video/mp4',
-		},{
-			path: '//storage.googleapis.com/aries-website/hero_video/SampleVideoForSite_ARIES.webm',
-			type: 'video/webm',
-		}]
-	}];
-
 	$scope.video = videos[Math.floor(Math.random() * videos.length)];
 
 	$scope.search = function(){
@@ -37,4 +36,18 @@ angular.module('ariesautomotive').controller('HomepageNavController', ['$scope',
 
 		els[0].classList.remove('hover');
 	};
+
+	$scope.checkScreenWidth = function() {
+		if (document.body.clientWidth > 450) {
+			$scope.showVideo = true;
+		} else {
+			$scope.showVideo = false;
+		}
+	}
+
+	$scope.checkScreenWidth();
+
+	window.addEventListener('resize', function () {
+		$scope.checkScreenWidth();
+	}, false);
 }]);
